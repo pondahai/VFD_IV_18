3fda884b2f658a8ec0a12ebf95a2b0543911211e
pondahai <dahai.pon@gmail.com>
2023-01-06 00:10:32 +0800

    Add files via upload

--- software/IV-18-web.ino
+++ software/IV-18-web.ino
@@ -11,22 +11,35 @@ String serialData;
  * 
  */
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+//#include <WiFiClient.h>
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
 #include <uri/UriBraces.h>
 #include <uri/UriRegex.h>
 
+#include "RTClib.h"
+RTC_DS3231 rtc;
+
 #ifndef STASSID
 #define STASSID "238H8F"
 #define STAPSK  "0920360270"
 #endif
 
+#ifndef APSSID
+#define APSSID "ESP_VFD_Clock"
+#define APPSK "00000000"
+#endif
+
 const char *ssid = STASSID;
 const char *password = STAPSK;
+
+const byte DNS_PORT = 53;
+IPAddress apIP(172, 217, 28, 1);
 IPAddress ip;  
-ESP8266WebServer server(80);
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
 /*
  * 
  */
@@ -39,19 +52,127 @@ String ipToString(IPAddress ip){
 /*
  * 
  */
-String body = " <!DOCTYPE html> "
+String responseHTML = "<meta charset='utf-8'>"
+"<style>"
+"body {"
+"  background: #ffff00;"
+"  font-size: 96px !important;"
+"}"
+".all-news-btn {"
+"  "
+"  background: #666666;"
+"  "
+"  background-image: -webkit-linear-gradient(top, #666666, #333333);"
+"  background-image: -moz-linear-gradient(top, #666666, #333333);"
+"  background-image: -ms-linear-gradient(top, #666666, #333333);"
+"  background-image: -o-linear-gradient(top, #666666, #333333);"
+"  background-image: linear-gradient(to bottom, #666666, #333333);"
+"  "
+"  -webkit-border-radius: 3;"
+"  -moz-border-radius: 3;"
+"  border-radius: 3px;"
+"  "
+"  font-family: Arial !important;"
+"  color: #ffffff !important;"
+"  font-size: 96px !important;"
+"  padding: 7px 8px 7px 7px;"
+"  text-decoration: none;"
+"  margin-left: 3px;"
+""
+"  min-width: 100px;"
+"  min-height: 30px;"
+"  display: inline;"
+"}"
+""
+".all-news-btn:hover {"
+"  background: #666666;"
+"  background-image: -webkit-linear-gradient(top, #666666, #000000);"
+"  background-image: -moz-linear-gradient(top, #666666, #000000);"
+"  background-image: -ms-linear-gradient(top, #666666, #000000);"
+"  background-image: -o-linear-gradient(top, #666666, #000000);"
+"  background-image: linear-gradient(to bottom, #666666, #000000);"
+"  text-decoration: none;"
+"}"
+""
+" @media only screen and (max-width:480px) { "
+"  .all-news-btn {width: 100%; font-size: 4.25em; }"
+"}"
+"</style>"
+""
+"<script> "
+" var timer = null;"
+"function start_timer() {"
+"  const now = new Date();   "
+"  const nowDay = now.getDay() + 1;"
+"  const nowMonth = now.getMonth() + 1;"
+"  const current = now.getFullYear().toString() + '-' + nowMonth.toString().padStart(2,' ') + '-' + nowDay.toString().padStart(2,' ') + 'T' + now.getHours().toString().padStart(2,' ') + ':' + now.getMinutes().toString().padStart(2,' ') + ':' + now.getSeconds().toString().padStart(2,' ');    "
+"   document.getElementById('demo').innerHTML =  current;    "
+"   const Http = new XMLHttpRequest(); "
+"  const url='/clock/'+current;   "
+"  Http.open('GET', url);   "
+"  Http.send();   "
+" timer = setInterval(function() {    "
+"  const now = new Date();   "
+"  const nowDay = now.getDay() + 1;"
+"  const nowMonth = now.getMonth() + 1;"
+"  const current = now.getFullYear().toString() + '-' + nowMonth.toString().padStart(2,' ') + '-' + nowDay.toString().padStart(2,' ') + 'T' + now.getHours().toString().padStart(2,' ') + ':' + now.getMinutes().toString().padStart(2,' ') + ':' + now.getSeconds().toString().padStart(2,' ');    "
+"   document.getElementById('demo').innerHTML =  current;    "
+"   const Http = new XMLHttpRequest(); "
+"  const url='/clock/'+current;   "
+"  Http.open('GET', url);   "
+"  Http.send();   "
+"   if (distance < 0) {     "
+"    clearInterval(x);     "
+"    document.getElementById('demo').innerHTML = 'EXPIRED';   "
+"    } "
+"    }, 10000);"
+"}"
+""
+"function active_clock() {"
+"  if (timer == null) {"
+"    start_timer();"
+"    return;"
+"  }"
+"} "
+""
+"function stop_timer_and_send_text() {"
+"  if (timer !== null) {"
+"    clearInterval(timer);"
+"    timer = null;"
+"    document.getElementById('demo').innerHTML = '';"
+"  }"
+"  const Http = new XMLHttpRequest();"
+"  const url='/text/'+document.getElementById('inputText').value;"
+"  Http.open('GET', url);   "
+"  Http.send();   "
+""
+"}"
+"</script> "
+""
+"<html><head></head>"
+"<body>"
+"<input id='inputText' class='all-news-btn' type='text' value='hello'/>"
+"<button onclick = 'stop_timer_and_send_text()' class='all-news-btn'>傳送</button>"
+"<p>"
+"<button onclick ='active_clock()' class='all-news-btn'>時鐘</button>"
+"<p>"
+"<h1><p id='demo'></p></h1>"
+"</body>"
+"</html>";
+ 
+String responseHTML_ = " <!DOCTYPE html> "
 "<html> "
 "<title></title> "
 "<head> </head> "
 "<body> "
 "<h1>Online HTML Editor</h1> "
-"<p id='demo'></p> "
+"<h1><p id='demo'></p></h1> "
 "</body> "
 "</html>  "
 "<script> "
 " var x = setInterval(function() {    "
 "  const now = new Date();   "
-"  const current = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();   " 
+"  const current = now.getHours().toString().padStart(2,' ') + '.' + now.getMinutes().toString().padStart(2,' ') + '.' + now.getSeconds().toString().padStart(2,' ');   " 
 "   document.getElementById('demo').innerHTML =  current;  "  
 "   const Http = new XMLHttpRequest(); "
 "  const url='/clock/'+current;   "
@@ -61,12 +182,97 @@ String body = " <!DOCTYPE html> "
 "    clearInterval(x);     "
 "    document.getElementById('demo').innerHTML = 'EXPIRED';   "
 "    } "
-"    }, 300); "
+"    }, 700); "
 "    </script> ";
- 
+
 /*
  * 
  */
+ unsigned char h2int(char c)
+{
+    if (c >= '0' && c <='9'){
+        return((unsigned char)c - '0');
+    }
+    if (c >= 'a' && c <='f'){
+        return((unsigned char)c - 'a' + 10);
+    }
+    if (c >= 'A' && c <='F'){
+        return((unsigned char)c - 'A' + 10);
+    }
+    return(0);
+}
+String urldecode(String str)
+{
+    
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    for (int i =0; i < str.length(); i++){
+        c=str.charAt(i);
+      if (c == '+'){
+        encodedString+=' ';  
+      }else if (c == '%') {
+        i++;
+        code0=str.charAt(i);
+        i++;
+        code1=str.charAt(i);
+        c = (h2int(code0) << 4) | h2int(code1);
+        encodedString+=c;
+      } else{
+        
+        encodedString+=c;  
+      }
+      
+      yield();
+    }
+    
+   return encodedString;
+}  
+/*
+ * 
+ */
+void cegc(void) {
+  analogWriteFreq(1046);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(1318);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(1567);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(2093);
+  analogWrite(15,127);
+  delay(100);
+  analogWrite(15,0);  
+}
+void cgec(void) {
+  analogWriteFreq(2093);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(1567);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(1318);
+  analogWrite(15,127);
+  delay(100);
+  analogWriteFreq(1046);
+  analogWrite(15,127);
+  delay(100);
+  analogWrite(15,0);    
+}
+void beep(){
+  analogWrite(15,127);
+  delay(333);
+  analogWrite(15,0);  
+}
+/*
+ * 
+ */
+int serialDataSubStart = 0;
+bool rtc_on = 0;
+
 void setup() 
 {
   /*
@@ -74,45 +280,122 @@ void setup()
    */
   Serial.begin(115200);
   
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+//  WiFi.mode(WIFI_STA);
+//  WiFi.begin(ssid, password);
+  /*
+   *  Wifi soft-AP
+   */
   Serial.println("");
+  Serial.print("Setting soft-AP ... ");
+  
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  boolean result = WiFi.softAP(APSSID);
+  
+  serialData = APSSID;
+  if(result == true)
+  {
+    Serial.println("Ready");
+  }
+  else
+  {
+    Serial.println("Failed!");
+  }
 
-
+  Serial.println("");
+  Serial.println(F(__DATE__));
+  Serial.println(F(__TIME__));
+  /*
+   * 
+   */
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+  }else{
+    if (rtc.lostPower()) {
+      Serial.println("RTC lost power, let's set the time!");
+      // When time needs to be set on a new device, or after a power loss, the
+      // following line sets the RTC to the date & time this sketch was compiled
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      // This line sets the RTC with an explicit date & time, for example to set
+      // January 21, 2014 at 3am you would call:
+      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    }
+    rtc_on = 1;    
+  }
+  
+  
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  ip = WiFi.localIP();
-  serialData = ipToString(ip);
-  Serial.println(ip);
+//  Serial.print("Connected to ");
+//  Serial.println(ssid);
+//  Serial.print("IP address: ");
+//  ip = WiFi.localIP();
+//  serialData = ipToString(ip);
+//  Serial.println(ip);
 
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
+  dnsServer.start(DNS_PORT, "*", apIP);
 
-  server.on(F("/"), []() {
-    server.send(200, "text/html", body);
+  webServer.onNotFound([]() {
+    webServer.send(200, "text/html", responseHTML);
   });
 
-  server.on(UriBraces("/clock/{}"), []() {
-    String user = server.pathArg(0);
-    server.send(200, "text/plain", "");
-    serialData = user;
+//  if (MDNS.begin("esp8266")) {
+//    Serisal.println("MDNS responder started");
+//  }
+
+  webServer.on(F("/"), []() {
+    webServer.send(200, "text/html", responseHTML);
   });
 
-  server.on(UriRegex("^\\/users\\/([0-9]+)\\/devices\\/([0-9]+)$"), []() {
-    String user = server.pathArg(0);
-    String device = server.pathArg(1);
-    server.send(200, "text/plain", "User: '" + user + "' and Device: '" + device + "'");
+  webServer.on(UriBraces("/clock/{}"), []() {
+    String user = webServer.pathArg(0);
+    webServer.send(200, "text/html", "<h1>"+urldecode(user)+"</h1>");
+    serialDataSubStart = 0;
+    serialData = urldecode(user);
+    String Year = serialData.substring(0,serialData.indexOf("-"));
+    String remain = serialData.substring(serialData.indexOf("-")+1,serialData.length());
+    String Month = remain.substring(0,remain.indexOf("-"));
+    String remain1 = remain.substring(remain.indexOf("-")+1,remain.length());
+    String Day = remain1.substring(0,remain1.indexOf("T"));
+    String remain2 = remain1.substring(remain1.indexOf("T")+1,remain1.length());
+    String Hour = remain2.substring(0,remain2.indexOf(":"));
+    String remain3 = remain2.substring(remain2.indexOf(":")+1,remain2.length());
+    String Min = remain3.substring(0,remain3.indexOf(":"));
+    String remain4 = remain3.substring(remain3.indexOf(":")+1,remain3.length());
+    String Sec = remain4;
+    Serial.print(Year.toInt());
+    Serial.print(" ");
+    Serial.print(Month.toInt());
+    Serial.print(" ");
+    Serial.print(Day.toInt());
+    Serial.print(" ");
+    Serial.print(Hour.toInt());
+    Serial.print(" ");
+    Serial.print(Min.toInt());
+    Serial.print(" ");
+    Serial.println(Sec.toInt());
+    rtc.adjust(DateTime(Year.toInt(),Month.toInt(),Day.toInt(),Hour.toInt(),Min.toInt(),Sec.toInt()));
   });
 
-  server.begin();
+  webServer.on(UriBraces("/text/{}"), []() {
+    String user = webServer.pathArg(0);
+    webServer.send(200, "text/html", "<h1>"+urldecode(user)+"</h1>");
+    serialDataSubStart = 0;
+    serialData = urldecode(user);
+  });
+
+//  server.on(UriRegex("^\\/users\\/([0-9]+)\\/devices\\/([0-9]+)$"), []() {
+//    String user = server.pathArg(0);
+//    String device = server.pathArg(1);
+//    server.send(200, "text/plain", "User: '" + user + "' and Device: '" + device + "'");
+//  });
+
+  webServer.begin();
   Serial.println("HTTP server started");
   /*
    * 
@@ -128,9 +411,18 @@ void setup()
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
 //  pinMode(blankPin, OUTPUT);
+  pinMode(15, OUTPUT);
+  analogWrite(15,127);
+  delay(1000);
+  analogWrite(15,0);
 }
-void iv18_digit(int digit, int num)
+/*
+ * 
+ */
+void iv18_digit(int digit, char ch)
 {
+  if(ch < 0x20 || ch > 0x7f) return;
+  
   unsigned char byte1,byte2,byte3;
   byte3=0xff;
 
@@ -156,10 +448,105 @@ void iv18_digit(int digit, int num)
   #define e 1<<3
   #define f 1<<6
   #define g 1<<4
-  int segment_convert_array[11]={a|b|c|d|e|f, b|c, a|b|g|e|d, 
-  a|b|g|c|d, f|g|b|c, a|f|g|c|d, 
-  a|f|g|c|d|e, a|b|c, a|b|c|d|e|f|g, a|b|c|d|f|g,0};
-  byte3 = segment_convert_array[num];
+  int segment_convert_array[0x7f-0x20+1]={
+    0,
+    b,
+    b|f,
+    b|c|e|f,
+    a|d|f|g,
+    a|c|d|f,
+    a|b|d|e|f|g,
+    f,
+    a|d|e|f,
+    a|b|c|d,
+    a|b|f|g,
+    e|f|g,
+    c|d,
+    g,
+    dp,
+    b|g,
+    a|b|c|d|e|f, // 0
+    b|c, 
+    a|b|g|e|d, 
+  a|b|g|c|d, 
+  f|g|b|c, 
+  a|f|g|c|d, 
+  a|f|g|c|d|e, 
+  a|b|c, 
+  a|b|c|d|e|f|g, 
+  a|b|c|d|f|g,  // 9
+  d|g,
+  c|d|g,
+  a|f|g,
+  a|g,
+  a|b|g,
+  a|b|e|g,
+  a|b|c|d|e|g,
+  a|b|c|e|f|g, // A
+  c|d|e|f|g,
+  a|d|e|f,
+  b|c|d|e|g,
+  a|d|e|f|g,
+  a|e|f|g,
+  a|c|d|e|f,
+  c|e|f|g,
+  a|d|e,
+  b|c|d|e,
+  a|c|e|f|g,
+  d|e|f,
+  a|c|e,
+  a|b|c|e|f,
+  c|d|e|g,
+  a|b|e|f|g,
+  a|b|c|f|g,
+  a|b|e|f,
+  c|f|g,
+  d|e|f|g,
+  b|c|d|e|f,
+  b|e|f,
+  b|d|f,
+  b|c|e|f|g,
+  b|c|d|f|g,
+  d|g,       // Z
+  a|d|e|f,
+  f|g,
+  a|b|c|d,
+  a|b|f,
+  d,
+  0,
+  a|b|c|e|f|g, // A
+  c|d|e|f|g,
+  a|d|e|f,
+  b|c|d|e|g,
+  a|d|e|f|g,
+  a|e|f|g,
+  a|c|d|e|f,
+  c|e|f|g,
+  a|d|e,
+  b|c|d|e,
+  a|c|e|f|g,
+  d|e|f,
+  a|c|e,
+  a|b|c|e|f,
+  c|d|e|g,
+  a|b|e|f|g,
+  a|b|c|f|g,
+  a|b|e|f,
+  c|f|g,
+  d|e|f|g,
+  b|c|d|e|f,
+  b|e|f,
+  b|d|f,
+  b|c|e|f|g,
+  b|c|d|f|g,
+  d|g,       // Z
+  0,
+  0,
+  0,
+  0,
+  0  
+  };
+  byte3 = segment_convert_array[ch-0x20];
   //byte3 = 0x80;
   digitalWrite(latchPin, LOW);  // 送資料前要先把 latchPin 設成低電位
   shiftOut(dataPin, clockPin, MSBFIRST, byte1);  //
@@ -220,21 +607,51 @@ int high_zero_blank=1;
 //
 String resultData;
 unsigned long string_shift_delay_time = 0;
-int serialDataSubStart = 0;
+int old_softAPgetStationNum = 0;
 void loop() 
 {
+  if((millis()-tick) % 500 == 0){
+    if(WiFi.softAPgetStationNum() != old_softAPgetStationNum){
+      old_softAPgetStationNum = WiFi.softAPgetStationNum();
+      Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+      if(WiFi.softAPgetStationNum() > 0){
+        cegc();
+      }
+      if(WiFi.softAPgetStationNum() == 0){
+        serialDataSubStart = 0;
+        serialData = APSSID;
+        cgec();
+      }
+    }
+  }
   /*
    * 
    */
-   server.handleClient();
+   dnsServer.processNextRequest();
+   webServer.handleClient();
    /*  
     *   
     */
    if (Serial.available() > 0) {
       // read the incoming byte:
-      serialData = Serial.readStringUntil('\n');
-      n=serialData.toInt();
+      String serialData_;
+      serialData_ = Serial.readStringUntil('\n');
+      n=serialData_.toInt();
+      serialDataSubStart = 0;
+      serialData = urldecode(serialData_);
    }
+   if (WiFi.softAPgetStationNum() == 0 && rtc_on == 1) {
+      serialDataSubStart = 0;
+      DateTime now = rtc.now();
+//      String hour_str = ((String(now.hour()).length() == 1)?" ":"") + String(now.hour());
+//      String min_str = ((String(now.minute()).length() == 1)?" ":"") + String(now.minute());
+//      String sec_str = ((String(now.second()).length() == 1)?" ":"") + String(now.second());
+//      serialData = hour_str + " " + min_str + " " + sec_str;
+      char str_buffer[12];
+      sprintf(str_buffer,"%2d %2d %2d",now.hour(),now.minute(),now.second());
+      serialData = String(str_buffer);
+   }
+  
   int dgt;
 
   if(serialData.length()>0){
@@ -243,7 +660,7 @@ void loop()
      */
     
     if(string_shift_delay_time == 0 ) resultData = serialData;
-    if((millis() - string_shift_delay_time) > 1500){
+    if((millis() - string_shift_delay_time) > 800){
       string_shift_delay_time = millis();
      if(serialData.length()>8){
       if((serialDataSubStart + 8) > serialData.length()){
@@ -258,13 +675,12 @@ void loop()
      }
     }
     char ch = resultData.charAt(digit_selector);
-    if(isDigit(ch)){
-      dgt=String(ch).toInt();
-      iv18_digit(7-digit_selector,dgt);
+    if(isAscii(ch)){
+//      dgt=String(ch).toInt();
+      iv18_digit(7-digit_selector,ch);
       delay(1);
-    }else{
-      iv18_digit(7-digit_selector,10);
     }
+
     tick = millis()+2000;
   }else{
     /*
@@ -272,7 +688,7 @@ void loop()
      */
     dgt=fetch_digit(n,digit_selector);
     if(high_zero_blank && dgt==0){
-      iv18_digit(digit_selector,10); //blank
+      iv18_digit(digit_selector,' '); //blank
     }else{
       high_zero_blank=0;
       iv18_digit(digit_selector,dgt);
